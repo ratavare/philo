e7501265d9e1ee3ddaaf978a4e7f7bc0b2d95e8c
ratavare <rafaelftcardoso@gmail.com>
2023-11-24 18:15:33 +0000

    Corrected time bugs

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: ratavare <ratavare@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 16:48:30 by ratavare          #+#    #+#             */
-/*   Updated: 2023/11/24 16:46:02 by ratavare         ###   ########.fr       */
+/*   Updated: 2023/11/24 18:10:57 by ratavare         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@ void	eating(t_philo *philo, t_data *data)
 	if (philo->id % 2)
 	{
 		pthread_mutex_lock(&data->forks[philo->left_fork_id]);
-		print_action(philo->id, data, "has taken a fork (his)"); 
+		print_action(philo->id, data, "has taken a fork"); 
 		pthread_mutex_lock(&data->forks[philo->right_fork_id]);
-		print_action(philo->id, data, "has taken a fork (right)");
+		print_action(philo->id, data, "has taken a fork");
 	}
 	else
 	{
 		pthread_mutex_lock(&data->forks[philo->right_fork_id]);
-		print_action(philo->id, data, "has taken a fork (right)");
+		print_action(philo->id, data, "has taken a fork");
 		pthread_mutex_lock(&data->forks[philo->left_fork_id]);
-		print_action(philo->id, data, "has taken a fork (his)"); 
+		print_action(philo->id, data, "has taken a fork"); 
 	}
-	pthread_mutex_lock(&data->death_check);
 	print_action(philo->id, data, "is eating");
+	pthread_mutex_lock(&data->death_check);
 	philo->last_meal = get_time();
 	philo->eat_count++;
 	pthread_mutex_unlock(&data->death_check);
@@ -45,25 +45,16 @@ void	*routine(void *v_philo)
 
 	philo = (t_philo *)v_philo;
 	data = philo->data;
-	if (philo->id % 2)
-		ft_mssleep(10);
 	if (data->philo_num == 1)
-		return (print_action(philo->id, data, "has taken a fork (his)"), NULL);
+		return (print_action(philo->id, data, "has taken a fork"), NULL);
 	while (!check_dead_flag(data))
 	{
-		// pthread_mutex_lock(&data->death_check);
-		// if (!(data->dead_flag))
-		// {
-			// pthread_mutex_unlock(&data->death_check);
-			eating(philo, data);
-			if (philo->eat_count == data->max_meals)
-				break ;
-			print_action(philo->id, data, "is sleeping");
-			ft_mssleep(data->time_to_sleep);
-			print_action(philo->id, data, "is thinking");
-		// }
-		// else
-			// pthread_mutex_unlock(&data->death_check);
+		eating(philo, data);
+		if (philo->eat_count == data->max_meals)
+			break ;
+		print_action(philo->id, data, "is sleeping");
+		ft_mssleep(data->time_to_sleep);
+		print_action(philo->id, data, "is thinking");
 	}
 	return (NULL);
 }
@@ -83,7 +74,7 @@ void	supervisor(t_philo *philo, t_data *data)
 			if (get_time() - philo[i].last_meal >= \
 			(long long unsigned int)data->time_to_die)
 			{
-				print_action(philo->id, data, "died");
+				print_action(i, data, "died");
 				pthread_mutex_lock(&data->writing);
 				data->dead_flag = 1;
 				pthread_mutex_unlock(&data->writing);
@@ -93,9 +84,7 @@ void	supervisor(t_philo *philo, t_data *data)
 			else
 				pthread_mutex_unlock(&data->death_check);
 		}
-		if (data->dead_flag)
-			break ;
-		// usleep(225);
+		ft_mssleep(1);
 	}
 }
 
@@ -109,11 +98,11 @@ void	destroy(t_philo *philo, t_data *data)
 		pthread_join(philo[i].thread, NULL);
 		i++;
 	}
-	i = data->philo_num;
-	while (i > 0)
+	i = 0;
+	while (i < data->philo_num)
 	{
 		pthread_mutex_destroy(&data->forks[i]);
-		i--;
+		i++;
 	}
 	pthread_mutex_destroy(&data->writing);
 	pthread_mutex_destroy(&data->death_check);
@@ -124,17 +113,16 @@ int	philosophers(t_data *data)
 	int		i;
 	t_philo	*philo;
 
-	i = 0;
+	i = data->philo_num;
 	data->first_time = get_time();
 	philo = data->philosophers;
-	while (i < data->philo_num)
+	while (--i >= 0)
 	{
 		if (pthread_create(&(philo[i].thread), NULL, routine, &(philo[i])))
 			return (1);
 		pthread_mutex_lock(&data->death_check);
 		philo[i].last_meal = get_time();
 		pthread_mutex_unlock(&data->death_check);
-		i++;
 	}
 	supervisor(philo, data);
 	destroy(philo, data);
